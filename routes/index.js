bdf50759cf2e5cb48e7bdd830e32b7355d1ebae6
S-Mapuvire <152277770+S-Mapuvire@users.noreply.github.com>
2024-11-06 13:33:05 +0200

    Update index.js
    Changed accessDB to allow for either main tags or 'velcro' tags

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,27 @@ const router = express.Router()
 const mongodb = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-router.get('/', async(req, res)=>{
+router.get('/:id', async(req, res)=>{
     try {
-        const data = await getEntries()
+        const data = await getEntries(req.params.id)
         res.status(200).json(data)
     } catch (error) {
         res.status(500).json({error})
     }
 })
 
-async function accessDB(){
+async function accessDB(db){
     const client = await mongodb.MongoClient.connect(uri);
-    return client.db('Scrapbook').collection('Tags');
+    if(db == 0){
+        return client.db('Scrapbook').collection('Tags');
+    }
+    else if(db == 1){
+        return client.db('Scrapbook').collection('Velcro');
+    }
 }
 
-async function getEntries() {
-    const scrapbook = await accessDB();
+async function getEntries(mode) {
+    const scrapbook = await accessDB(mode);
     // explicit casting of id because otherwise it sends it as a string
     const tags = (await scrapbook.find({}).toArray());
     return tags;
