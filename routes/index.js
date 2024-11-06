446d78f3de0d88285bc42709b4d7a46b334e1fa9
S-Mapuvire <152277770+S-Mapuvire@users.noreply.github.com>
2024-11-06 13:20:33 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,13 @@ const router = express.Router()
 const mongodb = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-router.get('/:id', async(req, res)=>{
+router.get('/', async(req, res)=>{
     try {
-        const data = await getEntry(req.params.id)
+        const data = await getEntries()
         res.status(200).json(data)
     } catch (error) {
         res.status(500).json({error})
     }
-
 })
 
 async function accessDB(){
@@ -19,10 +18,10 @@ async function accessDB(){
     return client.db('Scrapbook').collection('Tags');
 }
 
-async function getEntry(id) {
+async function getEntries() {
     const scrapbook = await accessDB();
     // explicit casting of id because otherwise it sends it as a string
-    const tags = (await scrapbook.find({_id: Number(id)}).toArray())
+    const tags = (await scrapbook.find({}).toArray());
     return tags;
 }
 
